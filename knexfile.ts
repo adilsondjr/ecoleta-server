c165734594732c2016e4e3aa3a1f42acdf4ad345
Adilson Domiciano <adilsondjr@gmail.com>
2020-06-02 23:46:51 -0300

    Instalando o knex, criando as tabelas e migrations

---
+++ knexfile.ts
@@ -0,0 +1,13 @@
+import path from 'path'
+
+module.exports = {
+    client: 'sqlite3',
+    connection: {
+        //__dirname sempre retorna o path do arquivo
+        filename:path.resolve(__dirname, 'src', 'database', 'database.sqlite')
+    },
+    migrations: {
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+    },
+    useNullAsDefault: true
+}
