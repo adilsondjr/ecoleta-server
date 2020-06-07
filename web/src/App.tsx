464a08ccdf111d84e66770cb8bfcae14f6ff0a00
Adilson Jr <adilsondjr@gmail.com>
2020-06-07 19:20:53 -0300

    Adicionando o conceito de estado e imutabilidade do react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './Header'
 
 // JSX: Sintaxe de XML dentro do JavScript
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  function handleButtonClick() {
+    setCounter(counter + 1)
+  }
+
   return (
     <div>
       <Header title="Hello Wortd!"/>
 
-      <h1>Conteudo da app</h1>
+      <h1>{counter}</h1>
+      <button type="button" onClick={handleButtonClick}>Aumentar</button>
     </div>
   );
 }
