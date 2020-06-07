ba1157101431a7566a9496032f3d216c3975f8d7
Adilson Jr <adilsondjr@gmail.com>
2020-06-07 16:37:07 -0300

    criando uma rota para listar um ponto de coleta específico

--- src/controllers/pointsController.ts
+++ src/controllers/pointsController.ts
@@ -1,7 +1,30 @@
-import { Request, Response } from 'express'
+import { Request, Response, response } from 'express'
 import knex from '../database/connection'
 
 class PointsController {
+    async show(req: Request, res: Response) {
+        //const id = req.params.id - pode fazer assim e tbm tem a de baixo com desestruturação
+        const { id } = req.params
+
+        const point = await  knex('points').where('id', id).first()
+
+        if(!point) {
+            return res.status(400).json({ message: 'Point not found' })
+        }
+
+        /**
+         *  SELECT ITEMS.TITLE FROM ITEMS
+         *     jOIN POINT_ITEMS on ITEMS.ID = POINT_ITEMS.ITEM_ID
+         *    WHERE POINT_ITEMS.POINT_ID = {ID}
+         */
+        const items = await knex('items')
+            .join('point_items', 'items.id', '=', 'point_items.item_id')
+            .where('point_items.point_id', id)
+            .select('items.title')
+
+        return res.json({ point, items})
+    }
+
     async create(req: Request, res: Response) {
         //descontrução destruction assigment
         const {
