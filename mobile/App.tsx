34a8a8e36cc0f65ce66f5186255a75f35fadad87
Adilson Jr <adilsondjr@gmail.com>
2020-06-13 11:46:10 -0300

    Configurando a navegação entre paginas

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,6 +7,8 @@ import Home from './src/pages/Home'
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
 
+import Routes from './src/routes'
+
 export default function App() {
   const [fontsLoaded] = useFonts({
       Roboto_400Regular, Roboto_500Medium, Ubuntu_700Bold
@@ -19,7 +21,7 @@ export default function App() {
   return (
     <>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent></StatusBar>
-      <Home />
+      <Routes />
     </>
   );
 }
