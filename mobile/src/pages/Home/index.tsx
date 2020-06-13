d9f2491f2704c67b01fbfe31bbf7128c8e3ee1df
Adilson Jr <adilsondjr@gmail.com>
2020-06-13 11:13:16 -0300

    Concluindo a tela inicial do app

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Feather as Icon } from '@expo/vector-icons'
 import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native'
 import { RectButton } from 'react-native-gesture-handler'
 
 const Home = () => {
     return (
         <ImageBackground source={require('../../assets/home-background.png')} style={styles.container} imageStyle={{ width:274, height: 368 }}>
-            <View>
+            <View style={styles.main}>
                 <Image source={require('../../assets/logo.png')} />
                 <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
                 <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Text>
@@ -13,6 +14,9 @@ const Home = () => {
 
             <View style={styles.footer}>
               <RectButton style={styles.button} onPress={() => {}}>
+                <View style={styles.buttonIcon}>
+                  <Icon name="arrow-right" color="#FFF" size={24}></Icon>
+                </View>
                 <Text style={styles.buttonText}>
                   Entrar
                 </Text>
